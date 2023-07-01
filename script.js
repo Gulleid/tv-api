210a8aa5a9aaa54fe3c901e146c7d01fc181064d
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-07-01 13:22:22 +0300

    tv shows

--- script.js
+++ script.js
@@ -1,96 +1,46 @@
 
-
-// document.querySelector("#search").addEventListener("click",function(){
-
-
-//     // let apikey ="6d2435d08ec7b5590047c360305ac903"
-//     let tv= document.querySelector("#search").value
-
-    fetch(`https://api.tvmaze.com/search/shows?q=${tv}`)
-    
-//     .then((movies)=>{
-
-//      return res.json()
-
-//     })
-//   .then ((movies)=>{
-//     movies.map(movies=>{
-
-//       document.querySelector(".movie-card")
-//     })
-
-//     console.log(data)
-
-
-//     document.querySelector(".search-box").innerText= data.name
-
-//   })
-// })
-
-
-// function displayTVNames() {
-
-
-//   fetch('https://api.tvmaze.com/search/shows?q=${t}') // Replace with the actual URL of the data source
-//     .then(response => response.json())
-//     .then(data => {
-//       // Assuming the data is an array of TV names
-//       data.forEach(tvName => {
-//         console.log(tvName);
-//       });
-//     })
-//     .catch(error => {
-//       console.log('Error:', error);
-//     });
-// }
-
-
-// document.querySelector("#search").addEventListener("click",function(){
-
-//   let tvName= document.querySelector("#search").value
-  
-//   fetch(`https://api.tvmaze.com/search/shows?q=${tvName}`)
-//   .then(response => response.json())
-//   .then(data => {
-//     // Assuming the data is an array of TV names
-//     data.forEach(tvName => {
-//       console.log(tvName);
-
-
-    
-//     });
-//   })
-//   .catch(error => {
-//     console.log('Error:', error);
-//   });
-
-
-
-// })
-
 document.querySelector("#search").addEventListener("click", function() {
-  let tvNameInput = document.querySelector("#search-input").value;
+  let tvNameInput = document.querySelector(".search").value;
 
   fetch(`https://api.tvmaze.com/search/shows?q=${tvNameInput}`)
     .then(res => res.json())
     .then(data => {
-      // Assuming the data is an array of TV names
-      data.forEach(tv=> {
-        console.log(tv.show.name);
+      const movieCard = document.querySelector(".movie-card");
+      movieCard.innerHTML = ''; // Clear previous content
+
+      // Assuming the data is an array of TV shows
+      data.forEach(show => {
+        console.log(show.show.name);
+        console.log(show.show.image.original);
+        console.log(show.show.rating.average);
+
+        // Create elements and update the movie card with show details
+        const showContainer = document.createElement("div");
+        showContainer.className = "show-container";
+        movieCard.appendChild(showContainer);
+
+        const imageElement = document.createElement("img");
+        imageElement.src = show.show.image.original;
+        imageElement.style.width = "200px"; // Set desired width
+        imageElement.style.height = "150px"; // Set desired height
+        showContainer.appendChild(imageElement);
+
+        const showDetails = document.createElement("div");
+        showDetails.className = "show-details";
+        showContainer.appendChild(showDetails);
+
+        const showNameElement = document.createElement("h2");
+        showNameElement.innerText = show.show.name;
+        showDetails.appendChild(showNameElement);
+
+        const ratingElement = document.createElement("p");
+        ratingElement.innerText = `Rating: ${show.show.rating.average}`;
+        showDetails.appendChild(ratingElement);
+
+        // Other elements and their updates
       });
     })
     .catch(error => {
       console.log('Error:', error);
     });
 });
-
-
-
-
-
-
-
-
-
-
-
